f332fc1c01aa9a937e68e0c82b5c25e42f99b674
Victoria Paiva <victoriadelfino08@gmail.com>
2024-05-13 15:50:48 -0300

    final

--- main.js
+++ main.js
@@ -1,12 +1,25 @@
-let listaDeItens = []
+let listaDeItens = [];
+let itemAEditar;
 
 const form = document.getElementById('form-itens');
-const itensInput = document.getElementById('receber-item');
-const ulItens = document.getElementById('lista-de-itens');
-const ulItensComprados = document.getElementById('itens-comprados');
+const itensInput = document.getElementById('receber-item'); // input que recebe itens digitados
+const ulItens = document.getElementById('lista-de-itens');// ul que vai imprimir itens adicionado na lista
+const ulItensComprados = document.getElementById('itens-comprados'); // ul que vai receber itens já comprados
+const listaRecuperada = localStorage.getItem('listaDeItens');
+
+function atualizaLocalStorage(){
+    localStorage.setItem('listaDeItens', JSON.stringify(listaDeItens))// stringfy transforma elementos em dados tipo string
+}
+
+if(listaRecuperada){
+    listaDeItens = JSON.parse(listaRecuperada)//parse transforma elementos em dados do tipo JS
+    mostrarItem()
+} else{
+    listaDeItens = []
+}
 
 form.addEventListener('submit', function (evento) {
-    evento.preventDefault()
+    evento.preventDefault() // evento para previnir que o button de tipo submit não seja enviado
     salvarItem()
     mostrarItem()
     itensInput.focus()
@@ -15,13 +28,13 @@ form.addEventListener('submit', function (evento) {
 // função para salver o item no array
 function salvarItem() {
     const comprasItem = itensInput.value;
-    
+
     //método some: é usado para testar se pelo menos um dos elementos do array satisfaz uma condição especificada numa função de teste
     const checarDuplicado = listaDeItens.some((elemento) =>
-    elemento.valor.normalize('NFD').replace(/[\u0300-\u036f]/g, '').toLowerCase().replace(/\s+/g, ' ').trim() === comprasItem.normalize('NFD').replace(/[\u0300-\u036f]/g, '').toLowerCase().replace(/\s+/g, ' ').trim())
-    if (checarDuplicado){
+        elemento.valor.normalize('NFD').replace(/[\u0300-\u036f]/g, '').toLowerCase().replace(/\s+/g, ' ').trim() === comprasItem.normalize('NFD').replace(/[\u0300-\u036f]/g, '').toLowerCase().replace(/\s+/g, ' ').trim())
+    if (checarDuplicado) {
         alert('item já existe')
-    }else{
+    } else {
         listaDeItens.push({
             valor: comprasItem,
             checar: false
@@ -32,14 +45,14 @@ function salvarItem() {
 }
 
 // função para imprimir o item na tela
-function mostrarItem(){
-    ulItens.innerHTML= ``;
-    ulItensComprados.innerHTML= ``;
+function mostrarItem() {
+    ulItens.innerHTML = ``;
+    ulItensComprados.innerHTML = ``;
 
-    listaDeItens.forEach((elemento, index)=>{
+    listaDeItens.forEach((elemento, index) => {
 
         if (elemento.checar) {
-            ulItensComprados.innerHTML +=`
+            ulItensComprados.innerHTML += `
             <li class="item-compra is-flex is-justify-content-space-between" data-value="${index}">
             <div>
                 <input type="checkbox" checked class="is-clickable" />  
@@ -53,9 +66,12 @@ function mostrarItem(){
             ulItens.innerHTML += `<li class="item-compra is-flex is-justify-content-space-between" data-value="${index}">
             <div>
                 <input type="checkbox" class="is-clickable" />
-                <input type="text" class="is-size-5" value="${elemento.valor}"></input>
+                <input type="text" class="is-size-5" value="${elemento.valor}" ${index !== Number(itemAEditar) ? 'disabled' : ''}></input>
             </div>
+
             <div>
+            ${index === Number(itemAEditar) ? '<button onclick="salvarEdicao()"> <i class="fa-regular fa-floppy-disk is-clickable"></i></button>' : '<i class="fa-regular is-clickable fa-pen-to-square editar"></i>'}
+            
                 <i class="fa-solid fa-trash is-clickable deletar"></i>
             </div>
         </li>`
@@ -67,17 +83,45 @@ function mostrarItem(){
     const inputsCheck = document.querySelectorAll('input[type="checkbox"]')
 
     //forEach é utilizado para iterar sobre cada elemento (checkbox) encontrado e armazenado em inputsCheck. - Para cada checkbox (i), é adicionado um ouvinte de eventos para reagir a cliques (click).
-    inputsCheck.forEach(i=>{
+    inputsCheck.forEach(i => {
         // o método addEventListener é usado para adicionar um ouvinte de eventos de clique a cada checkbox.
-        i.addEventListener('click', (evento)=>{
+        i.addEventListener('click', (evento) => {
             // Quando um checkbox é clicado, a função fornecida é chamada.
             //A função então acessa evento.target, que é o elemento do DOM que foi clicado — neste caso, o checkbox específico.
             //evento.target.parentElement.parentElement: Esta expressão acessa o pai do pai do checkbox. - Isso significa que ela vai subir dois níveis na árvore do DOM a partir do checkbox clicado. - Este código pode ser usado, por exemplo, para manipular ou extrair informações de elementos que estão mais acima na hierarquia do DOM em relação ao checkbox.
             const valorDoElemento = evento.target.parentElement.parentElement.getAttribute('data-value');
-            
+
             listaDeItens[valorDoElemento].checar = evento.target.checked
-            console.log(listaDeItens[valorDoElemento].checar)
             mostrarItem();
         })
     })
-} 
+    const deletarObjetos = document.querySelectorAll('.deletar');
+
+    deletarObjetos.forEach(i => {
+        i.addEventListener('click', (evento) => {
+            valorDoElemento = evento.target.parentElement.parentElement.getAttribute('data-value');
+            listaDeItens.splice(valorDoElemento, 1)
+            mostrarItem()
+            alert(`Item deletado da lista.`)
+        })
+    })
+
+    const editarItens = document.querySelectorAll('.editar')
+    editarItens.forEach(i => {
+        i.addEventListener('click', (evento) => {
+            itemAEditar = evento.target.parentElement.parentElement.getAttribute('data-value');
+            mostrarItem()
+        });
+    });
+    atualizaLocalStorage()
+}
+
+function salvarEdicao() {
+    const itemEditado = document.querySelector(`[data-value="${itemAEditar}"] input[type="text"]`)
+    // console.log(itemEditado.value)
+    listaDeItens[itemAEditar].valor = itemEditado.value;
+    console.log(listaDeItens)
+
+    itemAEditar = -1;
+    mostrarItem()
+}
